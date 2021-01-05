e51b59fab4aa168f3876179111f8c2dce421e74c
Moritz Langner <moritz@MacBook-Pro-ML.local>
2021-01-05 09:47:22 +0100

    Initial commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ class Header extends Component {
     render() { 
         return ( 
             <div>
-                <h1 color=''>AttentionBoard</h1>
+                <h1 color=''>AttentionBoard Test2</h1>
                 <hr></hr>
             </div>
          );
